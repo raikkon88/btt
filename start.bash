d7c179232e02ff577a6de89b2f5771e0e4dacbea
Marc Sánchez <msanxes@gmail.com>
2016-10-31 11:35:49 +0100

    version prepared for btt enginyers

--- start.bash
+++
@@ -1,2 +0,0 @@
-source ~/btt/bttenv/bin/activate
-python /home/pi/bttenginyers/manage.py runserver 0.0.0.0:8000 &
--- startinit.bash
+++ startinit.bash
@@ -4,8 +4,8 @@
 
 echo "--> UPDGRADING RASPBERRY PI <--"
 # Actualitzem les eines
-#sudo apt-get update
-#yes | sudo apt-get upgrade
+sudo apt-get update
+yes | sudo apt-get upgrade
 echo "- INSTALLING WEB SERVER AND DEPENDENCIES"
 # Installem el servidor web per poder lliurar l'entorn de producció.
 sudo apt-get -y install apache2 libapache2-mod-wsgi python-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.5-dev tk8.5-dev python-tk
@@ -13,9 +13,9 @@ echo "--> UPDGRADED AND INSTALLATION COMPLETE <--"
 
 echo "- INSTALLING DEPENDENCIES INSIDE ENVIRONMENT"
 # Installem django
-pip install django
-pip install requests
-pip install Pillow
+sudo pip install django
+sudo pip install requests
+sudo pip install Pillow
 echo "- MIGRATING DATABASE"
 # Generem la base de dades i migrem 
 sudo python manage.py makemigrations bttviewer
@@ -31,7 +31,6 @@ sudo source bttenv/bin/activate
 # desactivem l'entorn virtual
 echo "--> SETTING UP THE APPLICATION <--"
 # Encenem l'aplicació per el port 8000
-#python manage.py runserver 0.0.0.0:8000 & 
 sudo cp ~/btt/btt.conf /etc/apache2/sites-enabled/btt.conf
 sudo rm -r ~/btt/btt.conf
 
@@ -51,12 +50,3 @@ sudo chown -R www-data ~/btt/static
 
 exit
 
-#echo "--> CONFIGURING APACHE 2 <--"
-#sudo mv 000-default.conf /etc/apache2/sites-available/000-default.conf
-#echo "- CHANGING SQLITE 3 PERMISSIONS"
-#sudo chmod 664 ~/bttenginyers/db.sqlite3
-#sudo chown :www-data ~/bttenginyers/db.sqlite3
-#sudo chown :www-data ~/bttenginyers
-#echo "- RESTARTING APACHE"
-#sudo systemctl restart apache2
-#echo "- APACHE RESTARTED"
