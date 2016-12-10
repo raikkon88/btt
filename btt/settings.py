b0691055524285f16920f94392835ff56f262cb9
Marc Sánchez <msanxes@gmail.com>
2016-12-10 13:07:49 +0100

    settings refets

--- btt/settings.py
+++ btt/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '66p9mwr-o&-rugk%3a_^q=%!4)-3l56z4v4hiu@sw2tlld9v7&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
